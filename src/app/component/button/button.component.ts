b2c3e88134c8b7d670883547fe6fe88b36a36314
Shikhar Bansal <shikharbansal007@gmail.com>
2024-02-03 20:57:57 +0530

    Added Task component and Items

--- src/app/component/button/button.component.ts
+++ src/app/component/button/button.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
+  @Input() text: string = '';
+  @Input() color: string = '';
+  @Output() btnClick = new EventEmitter();
 
+  onClick() {
+    console.log('Add');
+    this.btnClick.emit();
+  }
 }
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   title = 'Task Tracker';
-
+  toggleAddTask() {
+    console.log('Toggle task');
+  }
 }
